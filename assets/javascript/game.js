4868bf5e3ca664aa565fdee5cb8a3e5ba7cdbe1d
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-10-20 11:05:02 -0500

    Update object and add comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,8 @@
 
 // define hangman object
 var hangman = {
-  // Define dictionary
+  // define dictionary. I DO NOT handle case of currentWord below because these
+  // are limited to lowercase
   dictionary: [
     'acres', 'adult', 'advice', 'arrangement', 'attempt', 'border',
     'breeze', 'brick', 'calm', 'canal', 'cast', 'chose', 'claws', 'coach',
@@ -23,29 +24,41 @@ var hangman = {
     'tobacco', 'toy', 'trap', 'treated', 'tune', 'university', 'vapor',
     'vessels', 'wealth', 'wolf', 'zoo'
   ],
-
-  // define "constant" of total number of guesses
-  TOTAL_NUMBER_OF_GUESSES: 6,
-
-  // define wins and losses
+  // define number of guesses left. 6 because that is how many body parts the
+  // hangman has
+  numberOfGuessesLeft: 6,
+  // start at 0 wins, losses, numberOfCorrectGuesses
   wins: 0,
   losses: 0,
-
-  // set currentWord property - this will be populated later
+  numberOfCorrectGuesses: 0,
+  // initialize variables that will be used later
   currentWord: '',
-
-  // set gamePlayArray property - this will be populated later
   gamePlayArray: [],
-
-  // keep track of guesses
   guesses: [],
-
-  // set userInput property
   userInput: '',
 
-  // keep track of correct guesses
-  numberOfCorrectGuesses: 0,
+  // define generic print function. Most other print functions use this.
+  printTo: function(element, message) {
+    document.getElementById(element).innerHTML = message;
+  },
 
+  // print directions for the user
+  printDirections: function(directions) {
+    this.printTo(
+      'directions',
+      '<h2 class="hidden-xs visible-sm-* visible-md-* visible-lg-*">' +
+      directions + '</h2><h4 class="visible-xs-* hidden-sm hidden-md ' +
+      'hidden-lg"><strong>' + directions + '</strong></h4>'
+    );
+  },
+
+  // get the word the user will try to guess
+  getWord: function() {
+    this.currentWord =
+      this.dictionary[Math.floor(Math.random() * this.dictionary.length)];
+  },
+
+  // initialize gamePlayArray with blank lines for each character of the word
   initializeGamePlayArray: function() {
     for (var i = 0; i < this.currentWord.length; i++) {
       this.gamePlayArray.push('__');
@@ -53,7 +66,52 @@ var hangman = {
     return this.gamePlayArray;
   },
 
-  // function for checking that key input is alphabetic
+  // print the array with blanks and guesses
+  printGamePlayArray: function() {
+    this.printTo('game-play', this.gamePlayArray.join(' '));
+  },
+
+  // print number of wins
+  printWins: function() {
+    this.printTo('wins', '<p><strong>Wins:</strong> ' + this.wins + '</p>');
+  },
+
+  // print number of losses
+  printLosses: function() {
+    this.printTo('losses', '<p><strong>Losses:</strong> ' + this.losses +
+                 '</p>');
+  },
+
+  // print characters that have already been guessed
+  printGuesses: function() {
+    this.printTo('guesses', '<p><strong># of guesses remaining:</strong> ' +
+                 this.numberOfGuessesLeft + '</p><p><strong>Letters already ' +
+                 'guessed:' + '</strong></p>' + this.guesses.join(' '));
+  },
+
+  // print stats (the above functions)
+  printStats: function() {
+    this.printWins();
+    this.printLosses();
+    this.printGuesses();
+  },
+
+  // start the game
+  startGame: function() {
+      this.printTo('hangman',
+                   '<img class="col-xs-12" src="assets/images/Hangman-0.png">');
+      this.printTo('game-board', '<div class="lead text-success" id="wins">' +
+                   '</div><div class="lead text-danger" id="losses"></div>' +
+                   '<div id="game-play"></div><div style="height: 20px;">' +
+                   '</div><div class="lead text-warning" id="guesses"></div>');
+      this.printDirections('Choose a letter to make a guess');
+      this.getWord();
+      this.gamePlayArray = this.initializeGamePlayArray();
+      this.printGamePlayArray();
+      this.printStats();
+  },
+
+  // check if input is an alphabetic character
   isAlpha: function(input) {
     var keyA = 65;
     var keyZ = 90;
@@ -62,135 +120,91 @@ var hangman = {
     }
   },
 
-  validateInput: function(input) {
+  // validate the users input
+  validateUserInput: function(input) {
+    // do nothing if the user's input is not alphabetic
     if (!this.isAlpha(input.keyCode)) {
       return;
     }
 
-    userInput = String.fromCharCode(input.keyCode).toLowerCase();
+    this.userInput = String.fromCharCode(input.keyCode).toLowerCase();
 
-    if (this.guesses.indexOf(userInput) != -1) {
+    // alert the user if they have already chosen the letter
+    if (this.guesses.indexOf(this.userInput) != -1) {
       alert('You have already chosen that letter. Please choose another!');
       return;
     }
-    return userInput;
+    return this.userInput;
+  },
+
+  // replace blank lines with correct letter choice
+  updateGamePlayArray: function() {
+    for (var i = 0; i < this.currentWord.length; i++) {
+      if (this.currentWord[i] === this.userInput) {
+        this.gamePlayArray[i] = this.userInput;
+      }
+    }
+  },
+
+  // update the hangman image
+  printHangman: function() {
+    this.printTo('hangman',
+                 '<img class="col-xs-12" src="assets/images/Hangman-' +
+                 (this.guesses.length - this.numberOfCorrectGuesses) + '.png">');
   },
 
   updateGameWithGuess: function() {
     // if guess is in the word update the game board
     if (this.currentWord.indexOf(this.userInput) != -1) {
-      for (var i = 0; i < this.currentWord.length; i++) {
-        if (this.currentWord[i] === this.userInput) {
-          this.gamePlayArray[i] = this.userInput;
-        }
-      }
-        this.numberOfCorrectGuesses += 1;
-        this.printGamePlay();
+      this.updateGamePlayArray();
+      this.numberOfCorrectGuesses += 1;
+      this.printGamePlayArray();
+    // else update the hangman image
     } else {
-        this.printHangman();
+      this.numberOfGuessesLeft--;
+      this.printHangman();
     }
   },
 
-  // functions for printing to the DOM
-  printTo: function(element, message) {
-      document.getElementById(element).innerHTML = message;
-  },
-
-  printHangman: function() {
-    this.printTo('hangman',
-      '<img class="col-xs-12" src="assets/images/Hangman-' +
-      (this.guesses.length - this.numberOfCorrectGuesses) + '.png">');
-  },
-
-  printDirections: function(directions) {
-    this.printTo('directions',
-      '<h2 class="hidden-xs visible-sm-* visible-md-* visible-lg-*">' +
-      directions + '</h2><h4 class="visible-xs-* hidden-sm hidden-md ' +
-      'hidden-lg"><strong>' + directions + '</strong></h4>');
-  },
-
-  printWins: function() {
-    this.printTo('wins', '<p><strong>Wins:</strong> ' + this.wins + '</p>');
-  },
-
-  printLosses: function() {
-    this.printTo('losses', '<p><strong>Losses:</strong> ' + this.losses +
-                 '</p>');
-  },
-
-  printGamePlay: function() {
-    this.printTo('game-play', this.gamePlayArray.join(' '));
-  },
-
-  printGuesses: function() {
-    var guessesRemaining =
-      (this.TOTAL_NUMBER_OF_GUESSES -
-        (this.guesses.length - this.numberOfCorrectGuesses));
-    this.printTo('guesses', '<p><strong># of guesses remaining:</strong> ' +
-      guessesRemaining + '</p><p><strong>Letters already guessed:</strong></p>' +
-      this.guesses.join(' '));
-  },
-
-  printBlankBoard: function() {
-    this.printTo('hangman',
-      '<img class="col-xs-12" src="assets/images/Hangman-0.png">');
-    this.printTo('game-board', '<div class="lead text-success" id="wins">' +
-      '</div><div class="lead text-danger" id="losses">' +
-      '</div><div id="game-play"></div><div style="height: 20px;">' +
-      '</div><div class="lead text-warning" id="guesses"></div>');
-  },
-
-  printStats: function() {
-      this.printWins();
-      this.printLosses();
-      this.printGuesses();
-  },
-
-  userHasGuessedWord: function() {
+  // check for winning result
+  userWon: function() {
     if (this.gamePlayArray.join('') === this.currentWord) {
       return true;
     }
   },
 
-  gameOver: function() {
-    if ((this.guesses.length - this.numberOfCorrectGuesses) >=
-        this.TOTAL_NUMBER_OF_GUESSES) {
+  // check for losing result
+  userLost: function() {
+    if (this.numberOfGuessesLeft <= 0) {
       return true;
     }
   },
 
-  // deal with results of game
+  // show the result to the user
+  showResult: function(divId, bootstrapClass, message) {
+    this.printDirections('Press any key to play again!');
+    this.printTo(divId, '<h1 class=' + bootstrapClass + '>' + message + '</h1>');
+  },
+
+  // check results
   checkResult: function() {
     // check for win and handle
-    if (this.userHasGuessedWord()) {
+    if (this.userWon()) {
       this.wins += 1;
       this.showResult('wins', 'text-success', 'You win!');
     }
 
     // check for loss and handle
-    if (this.gameOver()) {
+    if (this.userLost()) {
       this.losses += 1;
       this.printTo('game-play', 'the word was: ' + this.currentWord);
       this.showResult('losses', 'text-danger', 'You lose!');
     }
   },
 
-  showResult: function(divId, bootstrapClass, message) {
-    this.printDirections('Press any key to play again!');
-    this.printTo(divId,
-                 '<h1 class=' + bootstrapClass + '>' + message + '</h1>');
-  },
-
-  resetGame: function() {
-    this.numberOfCorrectGuesses = 0;
-    this.guesses = [];
-    this.gamePlayArray = [];
-    this.startGame();
-  },
-
-  // Get user input
+  // get input from the user
   getUserInput: function(event) {
-    this.userInput = this.validateInput(event);
+    this.userInput = this.validateUserInput(event);
     if (this.userInput === undefined) {
       return;
     }
@@ -201,28 +215,23 @@ var hangman = {
     this.checkResult();
   },
 
-  // start game
-  startGame: function() {
-    this.printBlankBoard();
-    this.printDirections('Choose a letter to make a guess');
-
-    // Get word
-    this.currentWord = this.dictionary[
-      Math.floor(Math.random() * this.dictionary.length)
-    ];
-
-    this.gamePlayArray = this.initializeGamePlayArray();
-    this.printGamePlay();
-    this.printStats();
+  // reset the game
+  resetGame: function() {
+    this.numberOfGuessesLeft = 6;
+    this.numberOfCorrectGuesses = 0;
+    this.guesses = [];
+    this.gamePlayArray = [];
+    this.startGame();
   }
 };
 
+// listen for keyup event. check condition of game to determine next step
 document.onkeyup = function(event) {
   if (hangman.currentWord === '') {
     hangman.startGame();
-  } else if (hangman.userHasGuessedWord() || hangman.gameOver()) {
-    return hangman.resetGame();
+  } else if (hangman.userWon() || hangman.userLost()) {
+    hangman.resetGame();
   } else {
-    return hangman.getUserInput(event);
+    hangman.getUserInput(event);
   }
 };
